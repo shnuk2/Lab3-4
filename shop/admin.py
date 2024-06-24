2f8d60683ace777da0a6477f4c65782fb9769f99
Yarolav2735 <yarolav9@gmail.com>
2024-06-25 01:46:03 +0300

    Delete shop directory

--- shop/admin.py
+++
@@ -1,8 +0,0 @@
-# src/mvc/admin.py
-
-from django.contrib import admin
-from shop.models import Product  # оновлено імпорт
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price')
